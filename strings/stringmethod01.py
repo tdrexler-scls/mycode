0279badce6a5c5689d5db3add357dcf15ac55ddc
tdrexler-scls <tdrexler@scls.info>
2022-10-03 21:55:50 +0000

    join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" docstring """
+
+# create a small string
+lilstring = "Alta3 Research offers classes on Python coding"
+newlist = lilstring.split(" ")
+print(newlist)
+
+# create a list of strings
+myiplist = ["192", "168", "0", "12"]
+# set singleip as the result of joining the list myiplist around the "."
+singleip = ".".join(myiplist)
+# display singleip
+print(singleip)
